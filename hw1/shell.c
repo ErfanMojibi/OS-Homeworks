a25f398b6b640b1e9bdb87ad5266481c14cb29b8
ce424-012-99105707 <mojibierfan@gmail.com>
2023-02-20 08:52:34 +0000

    redirect implemented but not working correctly

--- hw1/shell.c
+++ hw1/shell.c
@@ -7,6 +7,8 @@
 #include <termios.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define FALSE 0
 #define TRUE 1
@@ -17,7 +19,8 @@
 #include "process.h"
 #include "shell.h"
 
-int cmd_quit(tok_t arg[]) {
+int cmd_quit(tok_t arg[])
+{
   printf("Bye\n");
   exit(0);
   return 1;
@@ -28,34 +31,39 @@ int pwd();
 int cd(tok_t arg[]);
 int execute_program_with_absolute_path(tok_t arg[]);
 int execute_program(tok_t arg[]);
-int program_exists(char* path);
+int program_exists(char *path);
 /* Command Lookup table */
-typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token array and return int */
-typedef struct fun_desc {
+typedef int cmd_fun_t(tok_t args[]); /* cmd functions take token array and return int */
+typedef struct fun_desc
+{
   cmd_fun_t *fun;
   char *cmd;
   char *doc;
 } fun_desc_t;
 
 fun_desc_t cmd_table[] = {
-  {cmd_help, "?", "show this help menu"},
-  {cmd_quit, "quit", "quit the command shell"},
-  {pwd, "pwd", "print working directory"},
-  {cd, "cd", "change working directory"}
-};
+    {cmd_help, "?", "show this help menu"},
+    {cmd_quit, "quit", "quit the command shell"},
+    {pwd, "pwd", "print working directory"},
+    {cd, "cd", "change working directory"}};
 
-int cmd_help(tok_t arg[]) {
+int cmd_help(tok_t arg[])
+{
   int i;
-  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-    printf("%s - %s\n",cmd_table[i].cmd, cmd_table[i].doc);
+  for (i = 0; i < (sizeof(cmd_table) / sizeof(fun_desc_t)); i++)
+  {
+    printf("%s - %s\n", cmd_table[i].cmd, cmd_table[i].doc);
   }
   return 1;
 }
 
-int lookup(char cmd[]) {
+int lookup(char cmd[])
+{
   int i;
-  for (i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-    if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0)) return i;
+  for (i = 0; i < (sizeof(cmd_table) / sizeof(fun_desc_t)); i++)
+  {
+    if (cmd && (strcmp(cmd_table[i].cmd, cmd) == 0))
+      return i;
   }
   return -1;
 }
@@ -69,15 +77,17 @@ void init_shell()
       is not interactive */
   shell_is_interactive = isatty(shell_terminal);
 
-  if(shell_is_interactive){
+  if (shell_is_interactive)
+  {
 
     /* force into foreground */
-    while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
-      kill( - shell_pgid, SIGTTIN);
+    while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp()))
+      kill(-shell_pgid, SIGTTIN);
 
     shell_pgid = getpid();
     /* Put shell in its own process group */
-    if(setpgid(shell_pgid, shell_pgid) < 0){
+    if (setpgid(shell_pgid, shell_pgid) < 0)
+    {
       perror("Couldn't put the shell in its own process group");
       exit(1);
     }
@@ -92,7 +102,7 @@ void init_shell()
 /**
  * Add a process to our process list
  */
-void add_process(process* p)
+void add_process(process *p)
 {
   /** YOUR CODE HERE */
 }
@@ -100,108 +110,173 @@ void add_process(process* p)
 /**
  * Creates a process given the inputString from stdin
  */
-process* create_process(char* inputString)
+process *create_process(char *inputString)
 {
   /** YOUR CODE HERE */
   return NULL;
 }
 
-int pwd(){
+int pwd()
+{
   // printf("%s", get_current_dir_name());
-  char path[500]; //TODO size
-  if(getcwd(path, sizeof(path)) != NULL){
+  char path[500]; // TODO size
+  if (getcwd(path, sizeof(path)) != NULL)
+  {
     fprintf(stdout, "%s\n", path);
-  } else{
-    fprintf(stdout,"error occured");
+  }
+  else
+  {
+    fprintf(stdout, "error occured");
   }
   return 1;
 }
 
-int cd(tok_t arg[]){
-  if(chdir(arg[0]) != 0){
+int cd(tok_t arg[])
+{
+  if (chdir(arg[0]) != 0)
+  {
     fprintf(stdout, "error occured in changing directory\n");
   }
   return 1;
 }
-int program_exists(char* path){
-  if(!access(path, F_OK)){
-      return 1;
+int program_exists(char *path)
+{
+  if (!access(path, F_OK))
+  {
+    return 1;
   }
   return 0;
 }
-int execute_program(tok_t arg[]){
-  if(arg[0][0] == '/'){ // check for absolute path programs
-      execute_program_with_absolute_path(arg);
-  } else { 
-      char *sep = ":";
-      char *PATH = (char*) calloc(1024, 1); 
-      strcpy(PATH, getenv("PATH")); //get path
-      int flag = 0;
-      if(PATH){
-          char* token = strtok(PATH, sep);
-          while(token != NULL){
-            char read_path[500]; //TODO size
-
-            // concat program name to path
-            strcpy(read_path, token);
-            strcat(read_path, "/");
-            strcat(read_path, arg[0]);
-
-            // execute if program exists
-            if(program_exists(read_path)){
-              arg[0] = read_path;
-              execute_program_with_absolute_path(arg);
-              //free(PATH);
-              flag = 1;
-              break;
-            } else {
-              token = strtok(NULL, sep);
-            }
-          }
-        
-      }  else {
-          fprintf(stdout,"error in getting path\n");
-      }  
-      if(!flag)
-        fprintf(stdout,"program didn't exist\n");
-      
-      return 1;
+int execute_program(tok_t arg[])
+{
+  if (arg[0][0] == '/')
+  { // check for absolute path programs
+    execute_program_with_absolute_path(arg);
   }
+  else
+  {
+    char *sep = ":";
+    char *PATH = (char *)calloc(1024, 1);
+    strcpy(PATH, getenv("PATH")); // get path
+    int flag = 0;
+    if (PATH)
+    {
+      char *token = strtok(PATH, sep);
+      while (token != NULL)
+      {
+        char read_path[500]; // TODO size
 
-}
-int execute_program_with_absolute_path(tok_t arg[]){
-    int child_pid = fork();
-    if(child_pid == 0){
-      execv(arg[0], arg);
-    } else {
-      wait(child_pid);
-      return 1;
+        // concat program name to path
+        strcpy(read_path, token);
+        strcat(read_path, "/");
+        strcat(read_path, arg[0]);
+
+        // execute if program exists
+        if (program_exists(read_path))
+        {
+          arg[0] = read_path;
+          execute_program_with_absolute_path(arg);
+          // free(PATH);
+          flag = 1;
+          break;
+        }
+        else
+        {
+          token = strtok(NULL, sep);
+        }
+      }
     }
-    return 0;
+    else
+    {
+      fprintf(stdout, "error in getting path\n");
+    }
+    if (!flag)
+      fprintf(stdout, "program didn't exist\n");
+
+    return 1;
+  }
+}
+int execute_program_with_absolute_path(tok_t arg[])
+{
+  int child_pid = fork();
+  if (child_pid == 0)
+  {
+      execv(arg[0], arg);
+  }
+  else
+  {
+    wait(child_pid);
+    return 1;
+  }
+  return 0;
 }
 
-int shell (int argc, char *argv[]) {
-  char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
-  tok_t *t;			/* tokens parsed from input */
+int shell(int argc, char *argv[])
+{
+  char *s = malloc(INPUT_STRING_SIZE + 1); /* user input string */
+  tok_t *t;                                /* tokens parsed from input */
   int lineNum = 0;
   int fundex = -1;
-  pid_t pid = getpid();		/* get current processes PID */
-  pid_t ppid = getppid();	/* get parents PID */
+  pid_t pid = getpid();   /* get current processes PID */
+  pid_t ppid = getppid(); /* get parents PID */
   pid_t cpid, tcpid, cpgid;
 
   init_shell();
 
   // printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
 
-  lineNum=0;
+  lineNum = 0;
   // fprintf(stdout, "%d: ", lineNum);
-  while ((s = freadln(stdin))){
-    t = getToks(s); /* break the line into tokens */
+  while ((s = freadln(stdin)))
+  {
+    t = getToks(s);        /* break the line into tokens */
     fundex = lookup(t[0]); /* Is first token a shell literal */
-    if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
-    else {
+
+    // output redirect
+    int out_index = isDirectTok(t, ">");
+    int stdout_dup;
+    if(out_index != 0){
+        fflush(stdout);
+        stdout_dup = dup(fileno(stdout));
+        int user_file = open(t[out_index+1], O_CREAT|O_TRUNC|O_WRONLY, 0666);
+        dup2(user_file, fileno(stdout));
+        close(stdout);
+        t[out_index] = NULL;
+    }
+
+    //input redirect
+    int in_index = isDirectTok(t, "<");
+    int stdin_dup;
+    if(in_index != 0){
+      stdin_dup = dup(fileno(stdin));
+      int user_file = open(t[in_index+1], O_RDONLY|O_CREAT, 0666);
+      dup2(user_file, fileno(stdin));
+
+      fflush(stdin);
+      close(stdin);
+      t[in_index] = NULL;
+    }
+
+
+    if (fundex >= 0)
+      cmd_table[fundex].fun(&t[1]);
+    else
+    {
       execute_program(t);
-      //fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
+    }
+
+    // output redirect reset
+    if(out_index != 0){
+      dup2(stdout_dup, fileno(stdout));
+      close(stdout_dup);
+      fflush(stdout);
+    }
+
+    // input redirect reset
+    if(in_index != 0){
+      dup2(stdin_dup, fileno(stdin));
+      close(stdin_dup);
+      fflush(stdin);
     }
     // fprintf(stdout, "%d: ", lineNum);
   }
